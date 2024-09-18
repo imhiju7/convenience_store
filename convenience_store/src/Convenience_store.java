b6cd91b393353043e7a46ddfbd73b2ced380336d
imhiju7 <imhiju7@gmail.com>
2024-09-18 16:21:24 +0700

    tree

---
+++ convenience_store/src/Convenience_store.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Hieu PC
+ */
+public class Convenience_store {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
